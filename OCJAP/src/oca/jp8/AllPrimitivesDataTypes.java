44d7497e7af83cfb0588df437a7ccf94591010a4
Ashish Kumar <ashishkumar.4029@gmail.com>
2022-10-16 02:42:08 +0530

    first commit

---
+++ OCJAP/src/oca/jp8/AllPrimitivesDataTypes.java
@@ -0,0 +1,24 @@
+package oca.jp8;
+
+import java.util.Vector;
+
+public class AllPrimitivesDataTypes {
+    
+    public static void main(String[] args) {
+        //integers
+        byte bytePrim=100;
+        short shortPrim=10000;
+        int intPrim=100000;
+        long longPrim=10000000;
+        //fractional
+        float floatPrim=200;
+        double doublePrim=200000;
+        //binary
+        boolean boolPrim=false;
+        
+        System.out.println("byte "+bytePrim+"; short "+shortPrim+"; int "+intPrim+";  long "+longPrim+";");
+        System.out.println("float "+floatPrim+";   double "+doublePrim+";");
+        System.out.println("boolean "+boolPrim);
+    }
+
+}
